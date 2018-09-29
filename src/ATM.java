a6fb951de3227675a947853ad7294aad55aba5a2
Josh Schoep <joshuaschoep@mymail.mines.edu>
2018-09-28 23:47:19 -0600

    Built ATM class with bank account and handleTransaction method

--- src/ATM.java
+++ src/ATM.java
@@ -1,8 +1,22 @@
 
 public class ATM {
-
+	private BankAccount account1;
+	
 	public ATM() {
-		// TODO Auto-generated constructor stub
+		account1 = new BankAccount(500);
 	}
-
+	
+	public void handleTransactions() {
+		try {
+			account1.withdraw(600);
+		}catch(NegativeBalanceException e) {
+			System.out.println(e);
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void main(String[] args) {
+		ATM atm1 = new ATM();
+		atm1.handleTransactions();
+	}
 }
